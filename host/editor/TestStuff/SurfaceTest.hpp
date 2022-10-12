b4bcf010fee881e63aafe4ec8cd5560917506708
syarusinsky <siike92@hotmail.com>
2022-10-12 15:47:56 -0500

    Removed defines in favor of templates in Surface

--- host/editor/TestStuff/SurfaceTest.hpp
+++ host/editor/TestStuff/SurfaceTest.hpp
@@ -3,18 +3,18 @@
 
 #include "Surface.hpp"
 
-#define NUM_THREADS 1
+#define NUM_THREADS 4
 #define SHADER_PASS_DATA_SIZE 1024
 
 #ifdef SOFTWARE_RENDERING
-#define GRAPHICS SoftwareGraphics
+#define GRAPHICS_API SoftwareGraphics
 #else
-#define GRAPHICS OpenGlGraphics
+#define GRAPHICS_API OpenGlGraphics
 #endif // SOFTWARE_RENDERING
 
 void initStuff();
 
-class SurfaceTest : public Surface<640, 480, CP_FORMAT::RGB_24BIT, NUM_THREADS, false, SHADER_PASS_DATA_SIZE>
+class SurfaceTest : public Surface<RENDER_API::SOFTWARE, 640, 480, CP_FORMAT::RGB_24BIT, NUM_THREADS, true, SHADER_PASS_DATA_SIZE>
 {
 	public:
 		SurfaceTest();
@@ -23,7 +23,7 @@ class SurfaceTest : public Surface<640, 480, CP_FORMAT::RGB_24BIT, NUM_THREADS, 
 		void loadMesh1 (const std::string& filePath);
 		void loadMesh2 (const std::string& filePath);
 
-		void draw(GRAPHICS<640, 480, CP_FORMAT::RGB_24BIT, false, SHADER_PASS_DATA_SIZE>* graphics) override;
+		void draw(GRAPHICS_API<640, 480, CP_FORMAT::RGB_24BIT, true, SHADER_PASS_DATA_SIZE>* graphics) override;
 
 	private:
 		Mesh m_Mesh1;
